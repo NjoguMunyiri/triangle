1b088cc994cc2cb9d17b3d9b4cb99abd4bfc9bf0
NjoguMunyiri <munyiri_james@outlook.com>
2017-05-04 10:12:58 +0300

    declared valiables in js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
+$(document).ready(function() {
+  $("form").submit(function(event) {
 
-alert("fufu");
-
-{
-if (!(((a+b)<=c)||((a+c)<=b)||((b+c)<=a))) {
-    if (b===c&&c===a) {
-        show("Equilateral");
-    } else {
-      if (b===a||b===c||a===c) {
-        show("isosceles")
+  var a = parseInt($("#heightinput").val());
+  var b = parseInt($("#baseinput").val());
+  var c = parseInt($("#hypotenusinput").val());
+  alert(a);
+  alert(b);
+  alert(c);
+/*  {
+  if (!(((a+b)<=c)||((a+c)<=b)||((b+c)<=a))) {
+      if (b===c&&c===a) {
+          show("Equilateral");
       } else {
-        show("unequal sides");
+        if (b===a||b===c||a===c) {
+          show("isosceles");
+        } else {
+          show("unequal sides");
+        }
       }
-    }
-} else {
-    alert("Not a triangle");
-}
+  } else {
+      alert("Not a triangle");
+  }
 
-}
+}*/
+
+});
+});
